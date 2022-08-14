192d89f9ebe9ee64f73d30f6bd53d990d75909e9
uurha <uurh2man@gmail.com>
2022-08-15 00:52:44 +0200

    Prepare for upm

---
+++ Runtime/Extension/AsyncExtension/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace BetterExtensions.Runtime.Extension.AsyncExtension
+{
+    public static class ExtensionMethods
+    {
+        public static UnityWebRequestAwaiter GetAwaiter(this UnityWebRequestAsyncOperation asyncOp)
+        {
+            return new UnityWebRequestAwaiter(asyncOp);
+        }
+
+        public static AsyncOperationAwaiter GetAwaiter(this AsyncOperation asyncOp)
+        {
+            return new AsyncOperationAwaiter(asyncOp);
+        }
+        
+        public static AssetBundleRequestAwaiter GetAwaiter(this AssetBundleRequest asyncOp)
+        {
+            return new AssetBundleRequestAwaiter(asyncOp);
+        }
+    }
+}
---
+++ Runtime/Extension/ClipboardExtension.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace BetterExtensions.Runtime.Extension
+{
+    public static class ClipboardExtension
+    {
+        /// <summary>
+        /// Puts the string into the Clipboard.
+        /// </summary>
+        public static void CopyToClipboard(this string str)
+        {
+            GUIUtility.systemCopyBuffer = str;
+        }
+    }
+}
