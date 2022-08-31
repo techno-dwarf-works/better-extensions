62484a8aab9e23fce1c60f42edc470e13ce4cc24
uurha <uurh2man@gmail.com>
2022-08-31 02:54:51 +0200

    Add awaiter for UnityEvent and generic version

---
+++ Assets/Plugins/BetterExtensions/Runtime/Extension/ActionExtensions/ActionAwaiter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+
+namespace BetterExtensions.Runtime.Extension.ActionExtensions
+{
+    public static class ActionAwaiter
+    {
+        public static Task Await(this UnityEvent action, CancellationToken cancellationToken = default)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+            var buffer = new UnityEventWrapper(taskCompletionSource, ref action);
+            if (!cancellationToken.Equals(default))
+            {
+                cancellationToken.Register(() => buffer.Cancel());
+            }
+
+            return buffer.Initialize();
+        }
+        
+        public static Task Await<T>(this UnityEvent<T> action, CancellationToken cancellationToken = default)
+        {
+            var taskCompletionSource = new TaskCompletionSource<T>();
+            var buffer = new UnityEventWrapper<T>(taskCompletionSource, ref action);
+            if (!cancellationToken.Equals(default))
+            {
+                cancellationToken.Register(() => buffer.Cancel());
+            }
+
+            return buffer.Initialize();
+        }
+    }
+}
