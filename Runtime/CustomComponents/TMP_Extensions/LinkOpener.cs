4fb6a4d3065d0ba67f0f1e53ac25bc5fcf2fcb39
uurha <uurha@users.noreply.github.com>
2022-09-11 17:25:51 +0000

    Merge pull request #5 from uurha/dev
    Dev

---
+++ Runtime/CustomComponents/TMP_Extensions/LinkOpener.cs
@@ -0,0 +1,30 @@
+#if TEXTMESHPRO
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace BetterExtensions.Runtime.CustomComponents
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class LinkOpener : MonoBehaviour, IPointerClickHandler
+    {
+        private TMP_Text _tmp;
+
+        private void Awake()
+        {
+            _tmp = GetComponent<TMP_Text>();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            var linkIndex =
+                TMP_TextUtilities.FindIntersectingLink(_tmp, eventData.position,
+                    eventData
+                        .pressEventCamera); // If you are not in a Canvas using Screen Overlay, put your camera instead of null
+            if (linkIndex == -1) return; // was a link clicked?
+            var linkInfo = _tmp.textInfo.linkInfo[linkIndex];
+            Application.OpenURL(linkInfo.GetLinkID());
+        }
+    }
+}
+#endif
