1f72ac1bf5471974a96c52df4a98e7d6b2a2343e
uurha <uurh2man@gmail.com>
2022-10-21 23:51:44 +0200

    Add Awaiter for ResourceRequest

---
+++ Assets/Plugins/BetterExtensions/Runtime/Extension/AsyncExtension/ResourceRequestAwaiter.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace BetterExtensions.Runtime.Extension.AsyncExtension
+{
+    [DebuggerNonUserCode]
+    public readonly struct ResourceRequestAwaiter : INotifyCompletion
+    {
+        private readonly ResourceRequest _asyncOperation;
+        public bool IsCompleted => _asyncOperation.isDone;
+
+        public ResourceRequestAwaiter(ResourceRequest asyncOperation) => _asyncOperation = asyncOperation;
+
+        public void OnCompleted(Action continuation) => _asyncOperation.completed += _ => continuation();
+
+        public ResourceRequest GetResult()
+        {
+            return _asyncOperation;
+        }
+    }
+}
