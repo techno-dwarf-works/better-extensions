ce36482e8d21e3acc2f2619f65c803deb65952b8
uurha <uurh2man@gmail.com>
2022-11-28 21:57:29 +0100

    Minor update

--- Assets/Plugins/BetterExtensions/Runtime/Extension/NullableExtensions.cs
+++ Assets/Plugins/BetterExtensions/Runtime/Extension/NullableExtensions.cs
@@ -20,7 +20,7 @@ namespace BetterExtensions.Runtime.Extension
             if (!type.IsValueType) return true;
             return Nullable.GetUnderlyingType(type) != null;
         }
-        
+
         public static bool Cast<T>(this object o, out T value)
         {
             value = default;
@@ -31,7 +31,27 @@ namespace BetterExtensions.Runtime.Extension
                     return true;
                 }
             }
-            
+
+            if (o is T casted)
+            {
+                value = casted;
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool Cast<T1, T>(this T1 o, out T value)
+        {
+            value = default;
+            if (IsNullable<T>())
+            {
+                if (o == null)
+                {
+                    return true;
+                }
+            }
+
             if (o is T casted)
             {
                 value = casted;
--- Assets/Plugins/BetterExtensions/Runtime/Extension/UIExtension.cs
+++ Assets/Plugins/BetterExtensions/Runtime/Extension/UIExtension.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace BetterExtensions.Runtime.Extension
 {
