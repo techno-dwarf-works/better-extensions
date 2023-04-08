025a81d4958ec85859170bf32738a770cfb604ed
uurha <uurhaa@gmail.com>
2023-04-08 23:54:43 +0200

    Add some ReflectionExtensions

---
+++ Assets/BetterExtensions/Runtime/Extension/ReflectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Better.Extensions.Runtime
+{
+    public static class ReflectionExtensions
+    {
+        public static bool IsArrayOrList(this Type listType)
+        {
+            if (listType.IsArray)
+            {
+                return true;
+            }
+
+            if (listType.IsGenericType && listType.GetGenericTypeDefinition() == typeof(List<>))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static Type GetArrayOrListElementType(this Type listType)
+        {
+            if (listType.IsArray)
+            {
+                return listType.GetElementType();
+            }
+
+            if (listType.IsGenericType && listType.GetGenericTypeDefinition() == typeof(List<>))
+            {
+                return listType.GetGenericArguments()[0];
+            }
+            return null;
+        }
+    }
+}
