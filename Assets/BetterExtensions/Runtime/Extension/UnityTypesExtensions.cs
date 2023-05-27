601ad65bb795a96ef037135ab021bf89edc4ad18
uurhaa <uurha.dev@gmail.com>
2023-05-27 02:09:36 +0200

    Add UnityTypesExtensions.cs

---
+++ Assets/BetterExtensions/Runtime/Extension/UnityTypesExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Better.Extensions.Runtime
+{
+    public static class UnityTypesExtensions
+    {
+        public static bool IsNullOrDestroyed(this Object obj)
+        {
+            if (ReferenceEquals(obj, null)) return true;
+
+            return obj == null;
+        }
+    }
+}
