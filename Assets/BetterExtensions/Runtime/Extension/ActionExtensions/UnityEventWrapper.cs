2cd743b1b39fd1d87f10b1ceb50cbe12830e92f4
uurhaa <uurha.dev@gmail.com>
2023-07-20 00:20:25 +0200

    Split files

--- Assets/BetterExtensions/Runtime/Extension/ActionExtensions/UnityEventWrapper.cs
+++ Assets/BetterExtensions/Runtime/Extension/ActionExtensions/UnityEventWrapper.cs
@@ -31,38 +31,7 @@ namespace Better.Extensions.Runtime.ActionExtensions
             _taskCompletionSource.TrySetResult(result);
         }
     }
-    
-    internal class UnityEventDataWrapper<T> : BaseActionWrapper<T>
-    {
-        private UnityEvent _action;
-        private readonly T _data;
 
-        public UnityEventDataWrapper(TaskCompletionSource<T> taskCompletionSource, ref UnityEvent action, T data) : base(
-            taskCompletionSource)
-        {
-            _action = action;
-            _data = data;
-        }
-
-        public override Task<T> Initialize()
-        {
-            _action.AddListener(SetResult);
-            return _taskCompletionSource.Task;
-        }
-
-        public override void Cancel()
-        {
-            _action.RemoveListener(SetResult);
-            _taskCompletionSource.TrySetCanceled();
-        }
-
-        private void SetResult()
-        {
-            _action.RemoveListener(SetResult);
-            _taskCompletionSource.TrySetResult(_data);
-        }
-    }
-    
     internal class UnityEventWrapper : BaseActionWrapper<bool>
     {
         private UnityEvent _action;
