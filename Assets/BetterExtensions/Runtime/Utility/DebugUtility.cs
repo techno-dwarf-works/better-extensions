d7e0a3e5cde0f460fc430f993f084f690903b238
Anatoly Brizhan <anatoly.brizhan@gmail.com>
2024-02-24 10:06:58 +0100

    Add DebugUtility and ExceptionExtension

---
+++ Assets/BetterExtensions/Runtime/Utility/DebugUtility.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using Object = UnityEngine.Object;
+
+namespace Better.Extensions.Runtime
+{
+    public static class DebugUtility
+    {
+        [DebuggerHidden]
+        [DebuggerNonUserCode]
+        public static void LogException<T>(string message) where T : Exception, new()
+        {
+            var exception = new T();
+            exception.ReplaceExceptionMessageField(message);
+            Debug.LogException(exception);
+        }
+
+        [DebuggerHidden]
+        [DebuggerNonUserCode]
+        public static void LogException<T>(object message) where T : Exception, new()
+        {
+            LogException<T>(message.ToString());
+        }
+
+        [DebuggerHidden]
+        [DebuggerNonUserCode]
+        public static void LogException<T>(string message, Object context) where T : Exception, new()
+        {
+            var exception = new T();
+            exception.ReplaceExceptionMessageField(message);
+            Debug.LogException(exception, context);
+        }
+        
+        [DebuggerHidden]
+        [DebuggerNonUserCode]
+        public static void LogException<T>(object message, Object context) where T : Exception, new()
+        {
+            LogException<T>(message.ToString(), context);
+        }
+
+        [DebuggerHidden]
+        [DebuggerNonUserCode]
+        public static void LogException(string message)
+        {
+            var exception = new Exception(message);
+            Debug.LogException(exception);
+        }
+
+        [DebuggerHidden]
+        [DebuggerNonUserCode]
+        public static void LogException(object message)
+        {
+            LogException(message.ToString());
+        }
+
+        [DebuggerHidden]
+        [DebuggerNonUserCode]
+        public static void LogException(string message, Object context)
+        {
+            var exception = new Exception(message);
+            Debug.LogException(exception, context);
+        }
+
+        [DebuggerHidden]
+        [DebuggerNonUserCode]
+        public static void LogException(object message, Object context)
+        {
+            LogException(message.ToString(), context);
+        }
+    }
+}
