bb06c5ef12227933196fb7a1a3aa21ae5222236b
uurha <uurha@users.noreply.github.com>
2024-03-01 04:03:18 +0000

    Merge pull request #62
    SerializedPropertyExtensions

---
+++ Editor/Helpers/PropertyItemInfo.cs
@@ -0,0 +1,8 @@
+namespace Better.Extensions.EditorAddons
+{
+    public struct PropertyItemInfo
+    {
+        public string PropertyName { get; set; }
+        public int ElementIndex { get; set; }
+    }
+}
